b9f6e11894b0eb414fd1a1b362357a0c5448ca14
Ma'ayan Armony <maayan.armony@kcl.ac.uk>
2022-09-29 22:09:44 +0100

    Add exception for number of primes and review code

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f'number of primes={number_of_primes} must be a positive value.')
+
     list = []
+    count = 0
+    number = 2
+    while count < number_of_primes:
+        divisible = False
+        for i in range(2, number):
+            if number % i == 0:
+                break
+        else:
+            list.append(number)
+            count += 1
+        number += 1
     return list
